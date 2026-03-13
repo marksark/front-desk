6694182545b3430f24bdfc9dd6bc42c422720f83
Mark Sarkis <mark.sarkis93@gmail.com>
2026-03-13 11:13:26 -0700

    Hotfix vercel try be serve (#9)
    * try be service
    
    * update new path

--- server/src/index.ts
+++ server/src/index.ts
@@ -34,7 +34,7 @@ app.get("/api/health", (_req, res) => {
 app.use(express.static(path.join(__dirname, '../../client/dist')))
 
 // catch-all for React Router
-app.get('*', (_req, res) => {
+app.get('/{*path}', (_req, res) => {
   res.sendFile(path.join(__dirname, '../../client/dist/index.html'))
 })
 
